fe201f0ad953eb05a0dda7eea565713466ec9b4b
alonsojmateo <alonso.jmateo@gmail.com>
2025-05-07 20:22:01 -0300

    feat: codigo tarea uno

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv'
 // const alumnosRouter = require('./routes/alumnosRoutes')
 import alumnosRouter from './routes/alumnosRoutes.js'
+import ventasRouter from './routes/ventasRoutes.js'
 import conectarDB from './config/db.js';
 
 dotenv.config()
@@ -19,6 +20,7 @@ console.log("EL PUERTO ES: ", PORT);
 app.use(express.json()) // Para que pueda leer JSON
 
 app.use("/", alumnosRouter) // Manejar middlewares, nos permite conectar nuestro server
+app.use("/", ventasRouter)
 
 
 app.listen(PORT, () => {
