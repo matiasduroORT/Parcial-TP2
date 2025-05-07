c1bef16a95f8da81b73b7cbe79a9ef3d9b8c439b
alonsojmateo <alonso.jmateo@gmail.com>
2025-05-07 20:33:26 -0300

    feat: codigo tarea dos, endpoints negocios

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 // const alumnosRouter = require('./routes/alumnosRoutes')
 import alumnosRouter from './routes/alumnosRoutes.js'
 import ventasRouter from './routes/ventasRoutes.js'
+import negocioRouter from './routes/negociosRoutes.js'
 import conectarDB from './config/db.js';
 
 dotenv.config()
@@ -21,6 +22,7 @@ app.use(express.json()) // Para que pueda leer JSON
 
 app.use("/", alumnosRouter) // Manejar middlewares, nos permite conectar nuestro server
 app.use("/", ventasRouter)
+app.use("/", negocioRouter)
 
 
 app.listen(PORT, () => {
